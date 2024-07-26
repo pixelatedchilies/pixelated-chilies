bba16f189dd45f15050b224f5c4c6828f0a1879a
pixelatedchilies <info@pixelatedchilies.com>
2024-07-26 18:16:28 +0200

    Font family changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 import Navbar from "./views/Navbar";
 import Footer from "./components/Footer";
 import Script from "next/script";
-
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Pixelated Chilies",
@@ -19,14 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         {children}
+        <Footer />
         <Script
           src="https://static.heyflow.com/widget/latest/webview.js"
           strategy="afterInteractive"
         />
-        <Footer />
       </body>
     </html>
   );
