b555171b1840177c5aa260f2f97d0d41bb9efa77
pixelatedchilies <info@pixelatedchilies.com>
2024-07-26 18:23:49 +0200

    Footer titles

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer className="md:container md:mx-auto border border-slate-700 rounded-3xl mx-4 my-8">
-      <div className="footer text-base-content p-10">
+      <div className="footer text-base-content p-6">
         <nav>
-          <h6 className="font-semibold text-white">Services</h6>
+          <h6 className="font-semibold text-white text-lg">Services</h6>
           <a className="link link-hover">Web Design & Development</a>
           <a className="link link-hover">E-Commerce Shop</a>
           <a className="link link-hover">Social Media Management</a>
@@ -12,14 +12,14 @@ const Footer = () => {
           <a className="link link-hover">Social Recruiting</a>
         </nav>
         <nav>
-          <h6 className="font-semibold text-white">Company</h6>
+          <h6 className="font-semibold text-white text-lg">Company</h6>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Services</a>
           <a className="link link-hover">Help</a>
           <a className="link link-hover">Contact us</a>
         </nav>
         <nav>
-          <h6 className="font-semibold text-white">Legal</h6>
+          <h6 className="font-semibold text-white text-lg">Legal</h6>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Imprint</a>
