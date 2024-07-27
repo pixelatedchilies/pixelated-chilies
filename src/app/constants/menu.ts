6423abe4b7a5619e46ccd1aa24064347df033428
pixelatedchilies <info@pixelatedchilies.com>
2024-07-27 23:42:56 +0200

    Added about us page & Profile Cards

--- src/app/constants/menu.ts
+++ src/app/constants/menu.ts
@@ -6,7 +6,7 @@ interface MenuLinks {
 
 export const menuLinks: MenuLinks[] = [
   { id: 1, title: "Home", href: "/" },
-  { id: 2, title: "About us", href: "/" },
+  { id: 2, title: "About us", href: "/about-us" },
   { id: 3, title: "Our services", href: "/" },
   { id: 4, title: "Contact us", href: "/" },
 ];
