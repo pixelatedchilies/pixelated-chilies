90c64de1c6d8ea01e55428d0374fd70491adf7bc
pixelatedchilies <info@pixelatedchilies.com>
2024-08-04 16:20:07 +0200

    Animations added & code splitting

---
+++ src/app/components/Accordion.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export interface AccordionItem {
+  id: string;
+  title: string;
+  content: string[];
+}
+
+interface AccordionProps {
+  items: AccordionItem[];
+}
+
+const Accordion: React.FC<AccordionProps> = ({ items }) => (
+  <section className="flex flex-col gap-2">
+    {items.map((item) => (
+      <div
+        key={item.id}
+        className="collapse collapse-arrow border border-slate-700 hover:bg-base-200 transition duration-300"
+      >
+        <input type="radio" name="my-accordion" id={item.id} />
+        <label
+          htmlFor={item.id}
+          className="collapse-title text-lg text-white font-normal"
+        >
+          {item.title}
+        </label>
+        <div className="collapse-content">
+          {item.content.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </div>
+      </div>
+    ))}
+  </section>
+);
+
+export default Accordion;
---
+++ src/app/constants/solutionsList.ts
@@ -0,0 +1,33 @@
+import { ListItem } from "../ui/ReusableList";
+
+export const schedulingList: ListItem[] = [
+  { text: "Services" },
+  { text: "Calendar" },
+  { text: "Time slots" },
+  { text: "Business Locations" },
+  { text: "Business Agents" },
+  { text: "Live chat communication" },
+  { text: "View and manage Appointments" },
+  { text: "Working days & hours" },
+  { text: "Service Durations" },
+  { text: "QR Code Appointment Generator" },
+];
+
+export const paymentList: ListItem[] = [
+  { text: "Security and credibility" },
+  { text: "Multi-Currency support" },
+  { text: "Increase conversions" },
+  { text: "Workflow simplifier" },
+  { text: "Reduce cart abandonment" },
+  { text: "Pay in your preferred currency" },
+];
+
+export const productShowcaseList: ListItem[] = [
+  { text: "Payment Gateways" },
+  { text: "Reach wider audience" },
+  { text: "Comprehensive catalog" },
+  { text: "Faster purchases" },
+  { text: "Track logistics" },
+  { text: "Reduced costs" },
+  { text: "Scalability" },
+];
